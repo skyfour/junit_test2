40a19183ad0afe36f4d7353a8f89415170806458
skyfour <zhagjiap@163.com>
2018-05-07 17:26:01 +0800

    123

--- src/main/java/com/sky/dao/AgentUserHistoryDao.java
+++ src/main/java/com/sky/dao/AgentUserHistoryDao.java
@@ -4,5 +4,5 @@ import com.sky.beans.AgentUserHistory;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface AgentUserHistoryDao extends ElasticsearchRepository<AgentUserHistory, Long> {
-
+  
 }
